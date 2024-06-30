cdf82a03474d073f67bbc20d82b3d5b2b7e89793
cliff <clifflee1022@gmail.com>
2024-06-30 16:40:13 +0800

    feat: chapter4 Display Templates

---
+++ app/(data)/Templates.tsx
@@ -0,0 +1,107 @@
+export default [
+  {
+    name: 'Blog Title',
+    desc: 'An AI tool that generate blog title',
+    category: 'Blog',
+    icon: 'https://cdn-icons-png.flaticon.com/128/4186/4186534.png',
+    aiPrompt: 'Give me 5 blog topic idea in bullet wise only based',
+    slug: 'blog-title-generation',
+    form: [
+      {
+        label: 'Enter your blog niche',
+        field: 'input',
+        name: 'niche',
+        required: true
+      },
+      {
+        label: 'Enter blog outline',
+        field: 'textarea',
+        name: 'outline'
+      }
+    ]
+  },
+  {
+    name: 'Blog Content',
+    desc: 'An AI tool that generate blog title',
+    category: 'Blog',
+    icon: 'https://cdn-icons-png.flaticon.com/128/4186/4186534.png',
+    aiPrompt: 'Give me 5 blog topic idea in bullet wise only based',
+    slug: 'blog-content-generation',
+    form: [
+      {
+        label: 'Enter your blog niche',
+        field: 'input',
+        name: 'niche',
+        required: true
+      },
+      {
+        label: 'Enter blog outline',
+        field: 'textarea',
+        name: 'outline'
+      }
+    ]
+  },
+  {
+    name: 'Youtube SEO Title',
+    desc: 'An AI tool that generate blog title',
+    category: 'Blog',
+    icon: 'https://cdn-icons-png.flaticon.com/128/4186/4186534.png',
+    aiPrompt: 'Give me 5 blog topic idea in bullet wise only based',
+    slug: 'youtube-seo-title-generation',
+    form: [
+      {
+        label: 'Enter your youtube title',
+        field: 'input',
+        name: 'title',
+        required: true
+      },
+      {
+        label: 'Enter blog outline',
+        field: 'textarea',
+        name: 'outline'
+      }
+    ]
+  },
+  {
+    name: 'Youtube Description',
+    desc: 'An AI tool that generate blog title',
+    category: 'Blog',
+    icon: 'https://cdn-icons-png.flaticon.com/128/4186/4186534.png',
+    aiPrompt: 'Give me 5 blog topic idea in bullet wise only based',
+    slug: 'youtube-description-generation',
+    form: [
+      {
+        label: 'Enter your youtube title',
+        field: 'input',
+        name: 'title',
+        required: true
+      },
+      {
+        label: 'Enter blog outline',
+        field: 'textarea',
+        name: 'outline'
+      }
+    ]
+  },
+  {
+    name: 'Youtube Tags',
+    desc: 'An AI tool that generate blog title',
+    category: 'Blog',
+    icon: 'https://cdn-icons-png.flaticon.com/128/4186/4186534.png',
+    aiPrompt: 'Give me 5 blog topic idea in bullet wise only based',
+    slug: 'youtube-tags-generation',
+    form: [
+      {
+        label: 'Enter your youtube title',
+        field: 'input',
+        name: 'title',
+        required: true
+      },
+      {
+        label: 'Enter blog outline',
+        field: 'textarea',
+        name: 'outline'
+      }
+    ]
+  }
+]
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react"
+import SearchSection from "./_components/SearchSection"
+import TemplateListSection from "./_components/TemplateListSection"
 
 const Dashboard = () => {
+  const [userSearchInput, setUserSearchInput] = useState<string>()
+
   return (
-    <div>Dashboard</div>
+    <div>
+      {/* Search section */}
+      <SearchSection
+        onSearchInput={(value: string) => setUserSearchInput(value)}
+      />
+
+      {/* Template List Section */}
+      <TemplateListSection userSearchInput={userSearchInput}/>
+    </div>
   )
 }
 
