aa204c1a1d70b3b721760edc9b2516a89ac42096
cliff <clifflee1022@gmail.com>
2024-07-01 19:46:43 +0800

    feat: add landing page and dashboard/loading.tsx

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
-  '/',
 ]);
 
 export default clerkMiddleware((auth, req) => {
