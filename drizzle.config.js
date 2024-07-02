2831c4c788d62b0cdb158b5754f85563ad214d5a
cliff <clifflee1022@gmail.com>
2024-07-02 17:44:14 +0800

    fix: update

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,6 @@ export default {
   schema: "./utils/schema.ts",
   dialect: 'postgresql',
   dbCredentials: {
-    // url: process.env.DB_URL,
-    url: "postgresql://ai-content-generator_owner:iqA0IVDye2GN@ep-shy-waterfall-a5m17w56.us-east-2.aws.neon.tech/ai-content-generator?sslmode=require"
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
   }
 };
